eddf2fcef45f9066cf0184d0e58c03098b34e164
akvsy <104428673+akvsy@users.noreply.github.com>
2024-02-22 00:46:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 from student import Student
 from teacher import Teacher
 
-student1= Student("ayşe", "yılmaz")
-student2= Student("ali", "kaya")
-teacher1= Teacher("Nesibe", "Kaya", "Matematik")
-teacher2= Teacher("Ahmet", "Tanış", "Fizik")
-teacher3= Teacher("Özlem", "Akman", "Edebiyat")
-teacher4= Teacher("Kevser", "Yılmaz", "Kimya")
+ogrenciListesi=[]
+ogretmenListesi=[]
 
-ogrenciListesi= []
-ogretmenListesi= []
+def addStudent(isim,soyisim,vize,final):
+    newStudent=Student(isim,soyisim,vize,final)
+    ogrenciListesi.append(newStudent)
 
-def ogrenciEkle(ogrenci):
-    ogrenciListesi.append(ogrenci)
 
-ogrenciEkle(student1)
-print(ogrenciListesi)
+def printStudent():
+    for student in ogrenciListesi:
+        student.bilgi()
+        adSoyad= student.isim+ " " +student.soyisim
+        print(f"{adSoyad} ortalamanız:  {student.ortalamaHesaplama()}")
+
+def addTeacher(isim,soyisim,bolum,maas,zamOrani):
+    newTeacher=Teacher(isim,soyisim,bolum,maas,zamOrani)
+    ogretmenListesi.append(newTeacher)
+
+def printTeacher():
+    for teacher in ogretmenListesi:
+        teacher.bilgi()
+        print(f"Zamlı maaşınız=", teacher.zamliMaas())
+
+
+    
+
+addStudent("Nesibe","Kaya",70,80)
+addStudent("Kevser","Yılmaz",100,80)
+addTeacher("Özlem","Akman","Matematik",10000,30)
+addTeacher("Emir","Yılmaz","Fizik",12000,20)
+
+printStudent()
+printTeacher()
